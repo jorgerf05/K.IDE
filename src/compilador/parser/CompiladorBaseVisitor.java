6a9d859697bc72b65d7c9a79643b7d6170002738
jorgerf05 <jorgefco_rf@hotmail.com>
2022-10-23 22:31:15 -0700

    New parser files

---
+++ src/compilador/parser/CompiladorBaseVisitor.java
@@ -0,0 +1,127 @@
+// Generated from java-escape by ANTLR 4.11.1
+package compilador.parser;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link CompiladorVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class CompiladorBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements CompiladorVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCompilador(CompiladorParser.CompiladorContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCuerpo(CompiladorParser.CuerpoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArgs(CompiladorParser.ArgsContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitContenido(CompiladorParser.ContenidoContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitIf(CompiladorParser.IfContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCondicion(CompiladorParser.CondicionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitElse(CompiladorParser.ElseContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFor(CompiladorParser.ForContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAssignment(CompiladorParser.AssignmentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitDeclaration(CompiladorParser.DeclarationContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitImpresion(CompiladorParser.ImpresionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPotencia(CompiladorParser.PotenciaContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitNumero(CompiladorParser.NumeroContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMultiplicacion_o_division(CompiladorParser.Multiplicacion_o_divisionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitVariable(CompiladorParser.VariableContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSuma_o_resta(CompiladorParser.Suma_o_restaContext ctx) { return visitChildren(ctx); }
+}
---
+++ src/compilador/parser/CompiladorVisitor.java
@@ -0,0 +1,115 @@
+// Generated from java-escape by ANTLR 4.11.1
+package compilador.parser;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link CompiladorParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface CompiladorVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#compilador}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCompilador(CompiladorParser.CompiladorContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#cuerpo}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCuerpo(CompiladorParser.CuerpoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#args}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArgs(CompiladorParser.ArgsContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#contenido}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitContenido(CompiladorParser.ContenidoContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#if}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIf(CompiladorParser.IfContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#condicion}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCondicion(CompiladorParser.CondicionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#else}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitElse(CompiladorParser.ElseContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#for}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFor(CompiladorParser.ForContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code assignment}
+	 * labeled alternative in {@link CompiladorParser#assign}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAssignment(CompiladorParser.AssignmentContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#declaration}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDeclaration(CompiladorParser.DeclarationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link CompiladorParser#impresion}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitImpresion(CompiladorParser.ImpresionContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code potencia}
+	 * labeled alternative in {@link CompiladorParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPotencia(CompiladorParser.PotenciaContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code numero}
+	 * labeled alternative in {@link CompiladorParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitNumero(CompiladorParser.NumeroContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code multiplicacion_o_division}
+	 * labeled alternative in {@link CompiladorParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMultiplicacion_o_division(CompiladorParser.Multiplicacion_o_divisionContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code variable}
+	 * labeled alternative in {@link CompiladorParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariable(CompiladorParser.VariableContext ctx);
+	/**
+	 * Visit a parse tree produced by the {@code suma_o_resta}
+	 * labeled alternative in {@link CompiladorParser#expr}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSuma_o_resta(CompiladorParser.Suma_o_restaContext ctx);
+}
